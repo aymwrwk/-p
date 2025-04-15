f80720939ac59e4fe72d7b8686b3bd9ff3297ff6
Bruno Nelio <whosbin@gmail.com>
2025-04-15 13:56:57 -0300

    <

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-// Script para validar a senha e redirecionar para outra página
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Previne o envio do formulário padrão
-  
-    var password = document.getElementById('password').value;
-  
-    // Senha esperada
-    var correctPassword = '1234'; // Substitua por sua senha específica
-  
-    // Verifica se a senha está correta
-    if (password === correctPassword) {
-      // Redireciona para outra página (pode ser qualquer URL)
-      window.location.href = 'pagina-desejada.html'; // Substitua com a URL da página que você quer
-    } else {
-      alert('Senha incorreta!');
-    }
-  });
-  
+
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const senha = document.getElementById('password').value;
+  if (senha === '33') {
+    window.location.href = 'xyz.html';
+  } else {
+    alert("Senha incorreta!");
+  }
+});
